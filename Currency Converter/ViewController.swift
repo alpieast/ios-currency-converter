e243f8cf1ea2a75af2b9a9eac1bbdc2515e55bb1
Alperen <dogualperen@gmail.com>
2021-01-31 04:41:36 +0300

    showing currency symbols in UItableView

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -14,23 +14,75 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        parseRates()
+        CurrenciesTable.delegate = self
+        CurrenciesTable.dataSource = self
+        
+       
+    }
+    
+    @IBOutlet weak var CurrenciesTable: UITableView!
+    
+    var countedSymb = 0
+    var symbols = [String]()
+    var currencyWantedToConvert = String()
+    
+    
+    func parseRates(){
         AF.request("https://api.ratesapi.io/api/latest").responseJSON {response in
             switch response.result{
             case .success(let value):
                 let json = JSON(value)
+                let parsedRates = json["rates"]
                 
-                let EURUSD = json["rates"]["USD"]
-                let EURTRY = json["rates"]["TRY"]
-                debugPrint("1 EUR = "+"\(EURTRY)")
+                
+                
+                for (symbol,rate) in parsedRates{
+                    self.symbols.append(symbol)
+                    self.symbols.sort()
+                }
+                
+                print(self.symbols)
+                print(self.symbols.count)
+                self.countedSymb = self.symbols.count
+                self.CurrenciesTable.reloadData()
                 
                 
             case .failure(let error):
                 print(error)
             }
         }
-        
-       
     }
 
+    
+    func rateChangerBySelectedCurrency() {
+            }
 }
 
+extension ViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You Tapped me!")
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return countedSymb
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = symbols[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+}
